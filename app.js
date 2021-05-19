fb943af507428c0577d2eda2619069dc6de1372f
chandra193 <chandrakanth764@gmail.com>
2021-05-19 21:16:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", function (req,res) {
+
+    res.send("IT WORKED")
+    
+});
+
+app.listen(process.env.PORT || 5000)
+
+module.exports = app
